5bd13964cb3ebecea689e4bd8e065af2a3a02347
Anderson B Silva/ <32654298+oandersonbsilva@users.noreply.github.com>
2020-10-16 15:17:55 -0300

    Add files via upload

---
+++ Conexão-Remota.py
@@ -0,0 +1,7 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+
+server_address = ('192.168.0.110', 3066)    
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+print('Running server...')
+httpd.serve_forever()
